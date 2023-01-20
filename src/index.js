c0123555c63c547cfdad4e38f802a7553e0f3ee3
Chumak Slavik <chumakslav@gmail.com>
2023-01-20 23:59:22 +0200

    implemented library.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import {
-  fetchTrandingMovies,
-  searchMovies,
-  getMovieById,
-  getGenres,
-  getMovieVideo,
-} from './js/fetchMovies';
-import { renderHomePage, MOVIE_GENRES } from './js/card';
+import { fetchTrandingMovies } from './js/fetchMovies';
+import { renderHomePage } from './js/card';
 
 const refs = {
   gallery: document.querySelector('.cards'),
@@ -14,19 +8,3 @@ const refs = {
 fetchTrandingMovies(1).then(movies => {
   renderHomePage(refs.gallery, movies.results).then(response => {});
 });
-
-// searchMovies('Harry Potter', 1).then(movies => {
-//   console.log(movies);
-// });
-
-// getMovieById(315162).then(movie => {
-//   console.log(movie);
-// });
-
-// getGenres().then(genres => {
-//   console.log(genres);
-// });
-
-// getMovieVideo(550).then(movie => {
-//   console.log(movie);
-// });
