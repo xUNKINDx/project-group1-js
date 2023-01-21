4ee6f20cb6f31f1381cc3503de90e325e31e9d0d
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-01-21 18:56:31 +0200

    99%

--- src/js/markupModal.js
+++ src/js/markupModal.js
@@ -1,8 +1,6 @@
-export default function markupModal(data) {
-    const genreD = data.genres.map(genre => genre.name).join(", ");
-    const markup = `<img class="film-image" src="https://image.tmdb.org/t/p/original${data.poster_path}" alt=${data.title} loading="lazy"> 
-    <div class="film-info">
-        <div class="film-info__container">
+function getMarkupInfo(data) {
+    const genreD = data.genres.map(genre => genre.name).join(", ");   
+    const markup = `<div class="film-info__container">
             <h2 class="filmName" data-filmidmodal="">${data.title}</h2> 
             <div class="film-info__item">
                 <p class="film-info__itemName">Vote / Votes</p>
@@ -21,8 +19,14 @@ export default function markupModal(data) {
                 <p class="film-info__itemValue">${genreD}</p>
             </div>         
             <p class="film-info__desc">ABOUT<br>${data.overview}</p>
-        </div>
-    </div>`;
+        </div>`;
 
     return markup;
 }
+function getMarkupImage(data) {
+    const markup = `<img class="film-image" src="https://image.tmdb.org/t/p/original${data.poster_path}" alt=${data.title} loading="lazy">`
+    return markup;
+}
+
+
+export {getMarkupInfo,getMarkupImage};
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 import {getMovieById} from './fetchMovies.js';
-import markupModal from './markupModal.js';
+import {getMarkupInfo, getMarkupImage} from './markupModal.js';
 
+//localStorage.clear();
 
 const refs = {
     openModal: document.querySelector("[data-filmid]"),
@@ -9,8 +10,9 @@ const refs = {
     addToWatched: document.querySelector(".button-watched"),   
     addToQueue: document.querySelector(".button-queue"),  
     filmIdElem: document.querySelector("[data-filmidmodal]"), 
-    filmInfo: document.querySelector(".film-info__container"),
-    filmImage: document.querySelector(".film-image"),
+    filmCommon: document.querySelector(".film-container"),
+    filmInfoContainer: document.querySelector(".film-info__container"),
+    filmInfo: document.querySelector(".film-info"),    
   };
 
 const handleClick = (e) => {  
@@ -24,39 +26,65 @@ const handleClick = (e) => {
 }; 
 
 function getArrayFromLocalStorage(keyLocalStorage) {
-    let arrayFilms = [];    
+    let arrayFilms = []; 
+    let arrayIdFilms = []; 
+       
     const savedSettings = localStorage.getItem(keyLocalStorage); 
-        if(savedSettings !== null){
-            arrayFilms = savedSettings.split(";");     
-        };                
-    return arrayFilms;                               
+    
+    if(savedSettings !== null){
+        try {
+            arrayFilms = JSON.parse(savedSettings);
+            console.log("split");                     
+            arrayIdFilms = arrayFilms.map(film => film.id);           
+        }catch (e){
+            console.log("Ошибка парсинга из LocalStorage");
+        };    
+    }; 
+    const parsedSttings = {
+        arrayIdFilms: arrayIdFilms,
+        arrayFilms: arrayFilms, 
+    };  
+
+    return parsedSttings;                               
 };
 
 function addRemoveFilmsArrayInLocalStorage(arrayKeysLocalStorage,filmIdModal) {   
-    let keyLocalStorage = "";   
+    let keyLocalStorage = "";  
+    console.log(filmData); 
     for (let i = 0; i < arrayKeysLocalStorage.length; i++) {
         keyLocalStorage = arrayKeysLocalStorage[i]; 
-        arrayFilms = getArrayFromLocalStorage(keyLocalStorage);                     
-        if (arrayFilms.indexOf(filmIdModal) !== -1){ 
-            console.log(`Уже есть ${arrayFilms.join(";")}`);                
-            arrayFilms.splice(arrayFilms.indexOf(filmIdModal),1); 
-        }else{
-            arrayFilms.push(filmIdModal); 
+        const {arrayIdFilms, arrayFilms} = getArrayFromLocalStorage(keyLocalStorage);  
+        const indexFilm = arrayIdFilms.indexOf(parseInt(filmIdModal,10)); 
+        console.log(arrayIdFilms);
+        console.log(`indexFilm ${indexFilm}`);                    
+        if (indexFilm !== -1){ 
+            console.log(`Уже есть ${arrayIdFilms}`);                
+            arrayFilms.splice(indexFilm,1); 
+        }else{            
+            const dataForLocalStorage = {
+                poster_path: filmData.poster_path,
+                title: filmData.title,
+                genre_ids: filmData.genres,
+                id: filmData.id,
+                vote_average: filmData.vote_average,
+            };
+            arrayFilms.push(dataForLocalStorage); 
         };      
         if (keyLocalStorage === "Watched") {
             refs.addToWatched.classList.toggle("hasAlready");  
         } else {
             refs.addToQueue.classList.toggle("hasAlready");       
-        };                     
-        localStorage.setItem(keyLocalStorage, arrayFilms.join(";")); 
-        console.log(arrayFilms.join(";"));                            
+        };                   
+        localStorage.setItem(keyLocalStorage, JSON.stringify(arrayFilms));
+        console.log(`setLocal ${arrayFilms}`);                            
     };                               
 };
 
-function addClassBtnDependingOnStatus(keyLocalStorage,filmIdModal){       
-    arrayFilms = getArrayFromLocalStorage(keyLocalStorage);     
-    if (arrayFilms.indexOf(filmIdModal) !== -1){ 
-        console.log(`Уже есть ${arrayFilms.join(";")}`); 
+function addClassBtnDependingOnStatus(keyLocalStorage,filmIdModal){             
+    const {arrayIdFilms, arrayFilms}  = getArrayFromLocalStorage(keyLocalStorage);
+    const indexFilm = arrayIdFilms.indexOf(parseInt(filmIdModal,10));      
+    if (indexFilm !== -1){ 
+        console.log(`Уже есть ${arrayIdFilms}`); 
         if (keyLocalStorage === "Watched") {
             refs.addToWatched.classList.add("hasAlready");
         } else {
@@ -70,7 +98,12 @@ function removeClassBtnDependingOnStatus(){
     refs.addToQueue.classList.remove("hasAlready");                                 
 };
 
-const saveLocalStorageFilmStatus = (e) => {         
+const saveLocalStorageFilmStatus = (e) => { 
+       
+    if (filmData === null || filmData === undefined) {
+        return;
+    }
+
     const filmIdModal = refs.filmIdElem.dataset.filmidmodal;  
     let arrayKeysLocalStorage = [];    
     if (e.target.classList.contains("button-watched") === true) {
@@ -81,26 +114,33 @@ const saveLocalStorageFilmStatus = (e) => {
     addRemoveFilmsArrayInLocalStorage(arrayKeysLocalStorage,filmIdModal);
 };
 
+let filmData = null; 
 function toggleModal(e) {          
     refs.filmIdElem.dataset.filmidmodal = e.target.dataset.filmid;  
-    refs.modal.classList.toggle("is-hidden");        
+    refs.modal.classList.toggle("is-hidden");  
+      
+   
     if (refs.modal.classList.contains("is-hidden") === false) {
-        response = getMovieById(refs.filmIdElem.dataset.filmidmodal);
+        response = getMovieById(refs.filmIdElem.dataset.filmidmodal);        
         try {
             response.then((data) => {
-                console.log(data);
-                const markup = markupModal(data);
-                refs.filmInfo.innerHTML = "";
-                refs.filmImage.remove();
-                refs.filmInfo.insertAdjacentHTML("afterbegin",markup);               
+                filmData =  data; 
+                const markupImage = getMarkupImage(data);                               
+                const markupInfo = getMarkupInfo(data); 
+                const filmImage =  document.querySelector(".film-image");               
+                filmImage.remove();  
+                refs.filmCommon.insertAdjacentHTML("afterbegin",markupImage);               
+                refs.filmInfoContainer.innerHTML = "";             
+                refs.filmInfoContainer.insertAdjacentHTML("afterbegin",markupInfo);               
             });                
         } catch (error) {           
-           console.log(error);
+           console.log(error);        
         }
-        removeClassBtnDependingOnStatus();
+        removeClassBtnDependingOnStatus();      
+        document.addEventListener("keydown", handleClick);  
+        console.log('is-hidden false');  
         addClassBtnDependingOnStatus("Watched",refs.filmIdElem.dataset.filmidmodal);
-        addClassBtnDependingOnStatus("Queue",refs.filmIdElem.dataset.filmidmodal);
-        document.addEventListener("keydown", handleClick);               
+        addClassBtnDependingOnStatus("Queue",refs.filmIdElem.dataset.filmidmodal);            
         refs.addToWatched.addEventListener("click",saveLocalStorageFilmStatus);
         refs.addToQueue.addEventListener("click",saveLocalStorageFilmStatus);                             
     }else{        
