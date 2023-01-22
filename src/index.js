0a625dd2b3aab3a4d4a7110d50024927baa5f30d
Chumak Slavik <chumakslav@gmail.com>
2023-01-22 22:26:36 +0200

    merge modal

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './js/modal.js';
+import { fetchTrandingMovies } from './js/fetchMovies';
+import { renderHomePage } from './js/card';
+
+const refs = {
+  gallery: document.querySelector('.cards'),
+};
+
+fetchTrandingMovies(1).then(movies => {
+  renderHomePage(refs.gallery, movies.results).then(response => {});
+});
