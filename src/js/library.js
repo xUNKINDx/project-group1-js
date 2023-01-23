e842d8889f722e1459deed4067f680c1c8d66d75
Chumak Slavik <chumakslav@gmail.com>
2023-01-23 14:47:03 +0200

    library

--- src/js/library.js
+++ src/js/library.js
@@ -5,7 +5,17 @@ let mode = 'watched';
 
 const refs = {
   gallery: document.querySelector('.cards'),
+  watchedButton: document.querySelector('button#watched-button'),
+  queueButton: document.querySelector('button#queue-button'),
+  addToWatched: document.querySelector('.button-watched'),
+  addToQueue: document.querySelector('.button-queue'),
 };
+
+refs.watchedButton.addEventListener('click', selectWatched);
+refs.queueButton.addEventListener('click', selectQueue);
+refs.addToWatched.addEventListener('click', renderLibrary);
+refs.addToQueue.addEventListener('click', renderLibrary);
+
 
 renderLibrary();
 
